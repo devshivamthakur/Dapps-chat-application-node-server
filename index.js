666212f1cdcc87caecfc1989c2dea6b2ddb1d7ec
shivam thakur <64252021+shivam-kumarthakur@users.noreply.github.com>
2023-06-03 11:39:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-// in the index.js file
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const Gun = require('gun');
+const SEA = require('gun/sea'); // Required for encryption
+const crypto = require('crypto');
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT ||5000;
-const Gun = require('gun')
-
-app.use(Gun.serve)
-
+app.use(Gun.serve);
 
 const server = app.listen(port, () => {
-  console.log(`Gun server running on port ${port}🔥`)
-})
+  console.log(`Gun server running on port ${port}🔥`);
+});
 
-Gun({ web: server })
+const gun = Gun({ web: server });
+
+// Custom encryption and description based on userId
+gun.on('in', (msg) => {
+  const { '#': id, put } = msg;
+  const { userId, message } = put;
+  
+  if (msg.put && userId && message) {
+    const hashedUserId = crypto.createHash('sha256').update(userId).digest('hex');
+    const encryptedMessage = SEA.encrypt(message, hashedUserId);
+    const description = Encrypted message for user ${userId};
+
+    put.encryptedMessage = encryptedMessage;
+    put.description = description;
+  }
+});
