7b7732c9b6e9ff682180626112d392bece1b46ae
shivam thakur <64252021+shivam-kumarthakur@users.noreply.github.com>
2023-06-03 11:49:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const server = app.listen(port, () => {
 const gun = Gun({ web: server });
 
 // Custom encryption and description based on userId
-gun.on('in', (msg) => {
+gun.on('onSendMessage', (msg) => {
   const { '#': id, put } = msg;
   const { userId, message } = put;
   
@@ -25,4 +25,26 @@ gun.on('in', (msg) => {
   }
   
   
+});
+gun.on('message', async (msg) => {
+  const { '#': id, put, get } = msg;
+  const { chatroomId, userId, encryptedMessage, fileType } = put;
+
+  if (msg.put && chatroomId && userId && encryptedMessage && fileType === 'txt') {
+    const hashedUserId = crypto.createHash('sha256').update(userId).digest('hex');
+    const decryptedMessage = await SEA.decrypt(encryptedMessage, hashedUserId);
+    put.decryptedMessage = decryptedMessage;
+    gun.back(msg);
+  }
+  if (get && chatroomId && get === chatroomId) {
+    const messages = [];
+    gun.get(chatroomId).map().once((data) => {
+      if (data.fileType === 'txt' && data.decryptedMessage) {
+        messages.push(data.decryptedMessage);
+      }
+    });
+
+   gun.back(messages);
+    // Send the decrypted messages as a response or perform further actions
+  }
 });
